253f1f2b7216a1a0b58a4aff58b813fbff9b83dd
jonki324 <jonki324@gmail.com>
2020-02-19 03:03:07 +0900

    pug形式修正前

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
+  const now = new Date();
+  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
-  res.end();
+  
+  switch (req.method) {
+    case 'GET':
+      const fs = require('fs');
+      const rs = fs.createReadStream('./form.html');
+      rs.pipe(res);
+      break;
+    case 'POST':
+      let rawData = '';
+      req.on('data', (chunk) => {
+        rawData = rawData + chunk;
+      }).on('end', () => {
+        const decoded = decodeURIComponent(rawData);
+        console.info('[' + now + '] 投稿: ' + decoded);
+        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + decoded + 'が投稿されました</h1></body></html>');
+        res.end();
+      });
+      break;
+    case 'DELETE':
+      req.write('DELETE' + req.url);
+      break;
+    default:
+      break;
+  }
 }).on('error', (e) => {
   console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
